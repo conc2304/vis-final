8f95c5801ab04a068e2ce94a6dc99a6cd419e272
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2022-11-13 22:33:24 +0100

    router - nav menu

--- src/components/pages/Resources.tsx
+++ src/components/pages/Resources.tsx
@@ -2,26 +2,31 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 import Layout from '../ui/Layout';
+import Navigation from '../ui/Navigation/Navigation';
 
 const ResourcesPage = () => {
   return (
     <Layout>
       <main className="p-4 flex-grow-1 d-flex flex-column">
         <Row className="flex-grow-1">
-          <Col md={6}>{/* @TODO - add navigation */}</Col>
           <Col md={6}>
-            <h2>More Resources</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at imperdiet arcu.
-              Proin ut lacus mauris. Fusce et nisi iaculis, tincidunt mauris non, mollis nunc. Donec
-              euismod nulla id posuere laoreet. Proin dapibus elit mattis, placerat turpis ac,
-              ullamcorper neque. Mauris a erat egestas, vehicula nulla facilisis, bibendum sapien.
-              Vivamus sollicitudin aliquam purus id pharetra. Integer euismod neque quis leo
-              consectetur, rutrum commodo erat dapibus. Suspendisse maximus est venenatis, euismod
-              nibh bibendum, luctus mauris. Curabitur eu elementum nibh. Fusce porta vestibulum
-              dapibus. Vestibulum sed congue turpis. Nullam aliquam leo odio, nec congue lacus
-              placerat eget. Phasellus ut tincidunt libero.
-            </p>
+            <Navigation />
+          </Col>
+          <Col md={6} className="d-flex align-items-center">
+            <div>
+              <h2 className="pb-3">More Resources</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at imperdiet arcu.
+                Proin ut lacus mauris. Fusce et nisi iaculis, tincidunt mauris non, mollis nunc.
+                Donec euismod nulla id posuere laoreet. Proin dapibus elit mattis, placerat turpis
+                ac, ullamcorper neque. Mauris a erat egestas, vehicula nulla facilisis, bibendum
+                sapien. Vivamus sollicitudin aliquam purus id pharetra. Integer euismod neque quis
+                leo consectetur, rutrum commodo erat dapibus. Suspendisse maximus est venenatis,
+                euismod nibh bibendum, luctus mauris. Curabitur eu elementum nibh. Fusce porta
+                vestibulum dapibus. Vestibulum sed congue turpis. Nullam aliquam leo odio, nec
+                congue lacus placerat eget. Phasellus ut tincidunt libero.
+              </p>
+            </div>
           </Col>
         </Row>
       </main>
---
+++ src/components/ui/Navigation/Navigation.tsx
@@ -0,0 +1,24 @@
+import { Routes } from '../../../router/router';
+
+import NavigationLink, { NavigationLinkProps } from './NavigationLink';
+
+const Navigation = (): JSX.Element => {
+  const links: NavigationLinkProps[] = [
+    { linkTo: Routes.home, label: 'Home' },
+    { linkTo: Routes.storms, label: 'Severe Weather' },
+    { linkTo: Routes.hurricanes, label: 'Hurricanes' },
+    { linkTo: Routes.resources, label: 'Resources', active: true },
+  ];
+
+  return (
+    <nav className="bullet-navigation d-flex justify-content-center align-items-center h-100">
+      <ul className="d-flex flex-column p-0 list-unstyled">
+        {links.map((link) => (
+          <NavigationLink linkTo={link.linkTo} label={link.label} active={link.active} />
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
