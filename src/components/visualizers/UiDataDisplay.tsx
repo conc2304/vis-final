b92537bfb5c9b8b73ad66abf5d3cd1b6d6a3c033
Jose Conchello <jose.conchello@gmail.com>
2022-11-29 11:15:05 -0800

    clean up

--- src/components/visualizers/UiDataDisplay.tsx
+++ src/components/visualizers/UiDataDisplay.tsx
@@ -15,20 +15,6 @@ type Props = {
   };
 };
 
-function Number({ n, n0 = 0 }) {
-  const { number } = useSpring({
-    from: { number: n0 },
-    number: n,
-    duration: 300,
-    delay: 200,
-    config: {
-      mass: 1,
-      tension: 20,
-      friction: 10,
-    },
-  });
-  return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>;
-}
 
 const UiDataDisplay = ({
   timeRangeSelected = [YEAR_RANGE.min, YEAR_RANGE.max],
